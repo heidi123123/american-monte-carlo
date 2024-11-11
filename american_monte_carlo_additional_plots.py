3d016547be8f12a9a551123ba0ea7b47b0ce602b
Heidi Pang <heidi.pang@oepfelbaum.com>
2024-11-11 09:56:18 +0100

    slight fix to align x and y axis in asset path plot

--- american_monte_carlo_additional_plots.py
+++ american_monte_carlo_additional_plots.py
@@ -4,9 +4,8 @@ from american_monte_carlo import generate_asset_paths, get_quantlib_option, lsmc
 
 
 def plot_asset_paths(paths, T, n_time_steps, n_paths_to_plot=100):
-    # Plotting a subset of the generated paths
     plt.figure(figsize=(12, 6))
-    time_steps = np.linspace(0, T, n_time_steps)  # Generate time steps for the x-axis
+    time_steps = np.linspace(0, T, n_time_steps + 1)
 
     # Plot the first n_paths_to_plot paths as an example
     for i in range(n_paths_to_plot):
